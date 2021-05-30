230f0ddaac6d69bd096624923f0000a9e9cc6fe2
Eva813 <as45986@gmail.com>
2021-05-30 11:57:23 +0800

    js part let video play and pause

--- video-player                                  /script.js
+++ video-player                                  /script.js
@@ -1,7 +1,22 @@
 
+const video = document.querySelector('video');
+const progressRange = document.querySelector('.progress-range');
+const progressBar = document.querySelector('.progress-bar');
+const playBtn = document.getElementById('play-btn');
+const volumeIcon = document.getElementById('volume-icon');
+const volumeRange = document.querySelector(' volume-range');
+const volumeBar = document.querySelector(' volume-bar');
+const playerSpeed = document.querySelector('.player-speed');
 
 // Play & Pause ----------------------------------- //
-
+function togglePlay() {
+  if (video.paused) {
+    // 如果是暫停的，我們希望透過點擊使他play
+    video.play();
+  } else {
+    video.pause();
+  }
+}
 
 
 // Progress Bar ---------------------------------- //
@@ -19,3 +34,6 @@
 // Fullscreen ------------------------------- //
 
 
+playBtn.addEventListener("click", togglePlay);
+
+video.addEventListener("click", togglePlay);
