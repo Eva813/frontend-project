dd9d64e66d8c4c3769c95e8a0e8a5ff72bcd3b0d
Eva813 <as45986@gmail.com>
2021-05-30 15:35:17 +0800

    update progress bar

--- video-player                                  /script.js
+++ video-player                                  /script.js
@@ -9,19 +9,33 @@ const volumeBar = document.querySelector(' volume-bar');
 const playerSpeed = document.querySelector('.player-speed');
 
 // Play & Pause ----------------------------------- //
+
+function playIcon() {
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
+}
+
 function togglePlay() {
   if (video.paused) {
     // 如果是暫停的，我們希望透過點擊使他play
     video.play();
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    //改變title
+    playBtn.setAttribute('title', 'Pause');
   } else {
     video.pause();
+    playIcon();
   }
 }
 
-
+//當影片播到結束時，要顯示繼續播放的圖示
+playBtn.addEventListener("ended", playIcon);
 // Progress Bar ---------------------------------- //
 
-
+function updateProgress() {
+  console.log('currentTime', video.currentTime, 'duration', video.duration);
+  progressBar.style.width = `${(video.currentTime / video.duration) * 100}%`;
+}
 
 // Volume Controls --------------------------- //
 
@@ -35,5 +49,6 @@ function togglePlay() {
 
 
 playBtn.addEventListener("click", togglePlay);
-
 video.addEventListener("click", togglePlay);
+video.addEventListener('timeupdate', updateProgress);
+video.addEventListener('canplay', updateProgress)
